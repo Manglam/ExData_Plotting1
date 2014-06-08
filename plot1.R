f234e0f58cfdf3e47834abc45e64c7d1fa61ae06
Manglam <manglamtk@gmail.com>
2014-06-08 20:56:07 +0530

    plot1
    R code for plot1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+dt <- read.table("household_power_consumption.txt",header=TRUE,sep=';')
+dt$Date <- as.Date(as.character(dt$Date),"%d/%m/%Y")
+dt1 <- dt[dt$Date >= as.Date("2007-02-01") & dt$Date <= as.Date("2007-02-02"),]
+
+
+dt1$Global_active_power <- as.numeric(as.character(dt1$Global_active_power))
+
+hist((dt1$Global_active_power),col="red",main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+
+
+
