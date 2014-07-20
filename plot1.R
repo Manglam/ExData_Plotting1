e5c9d00a404d9b679ca28f8abbe2f89687388774
Manglam <manglamtk@gmail.com>
2014-07-20 20:21:47 +0530

    assigment1_added png save

--- plot1.R
+++ plot1.R
@@ -5,9 +5,12 @@ dt1 <- dt[dt$Date >= as.Date("2007-02-01") & dt$Date <= as.Date("2007-02-02"),]
 
 dt1$Global_active_power <- as.numeric(as.character(dt1$Global_active_power))
 
+png(filename = "plot1.png",
+    width = 480, height = 480)
 hist((dt1$Global_active_power),col="red",main="Global Active Power",
      xlab="Global Active Power (kilowatts)")
 
 
+dev.off()
 
 
--- plot2.R
+++ plot2.R
@@ -5,7 +5,17 @@ dt1 <- dt[dt$Date >= as.Date("2007-02-01") & dt$Date <= as.Date("2007-02-02"),]
 
 dt1$Global_active_power <- as.numeric(as.character(dt1$Global_active_power))
 
+png(filename = "plot2.png",
+    width = 480, height = 480)
+
+
 time <- paste(as.character(dt1$Date),as.character(dt1$Time),sep=" ")
 tim <- as.POSIXct(time)
 plot(tim,dt1$Global_active_power,type="l",xlab="",
      ylab="Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
+
